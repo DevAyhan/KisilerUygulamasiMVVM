731391e93ac586e703a83128935554f37a858fc3
DevAyhan <105974668+DevAyhan@users.noreply.github.com>
2022-09-17 19:58:35 +0300

    Initial commit

---
+++ app/src/main/java/com/example/kisileruygulamasimvvm/ui/viewmodel/AnasayfaViewModel.kt
@@ -0,0 +1,30 @@
+package com.example.kisileruygulamasimvvm.ui.viewmodel
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.kisileruygulamasimvvm.data.entity.Kisiler
+import com.example.kisileruygulamasimvvm.data.repo.KisilerDaoRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class AnasayfaViewModel @Inject constructor (var krepo:KisilerDaoRepository) : ViewModel() {
+
+    var kisilerListesi: MutableLiveData<List<Kisiler>>
+
+    init {
+        kisileriYukle()
+        kisilerListesi = krepo.kisileriGetir()
+    }
+    fun ara(aramaKelimesi:String){
+        krepo.kisiAra(aramaKelimesi)
+    }
+
+    fun sil(kisi_id:Int){
+        krepo.kisiSil(kisi_id)
+    }
+    fun kisileriYukle(){
+        krepo.tumKisileriAl()
+    }
+}
